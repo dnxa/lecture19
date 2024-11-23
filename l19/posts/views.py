ad11240f50938950ee647cfe92db1e989d50a4f4
dnxa <kuridze0@gmail.com>
2024-11-23 18:08:38 +0400

    Add responses for listing and creating posts with /posts and /posts/edit

--- l19/posts/views.py
+++ l19/posts/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse, HttpResponse
+from .models import Post
 # Create your views here.
-# main_posts, edit_posts
 
 def main_posts(request):
-    pass
+    if request.method == 'GET':
+        # Create a list with sets of data
+        result = [(i.title, i.description, i.author, i.comments, i.likes) for i in Post.objects.all()]
+
+        # Our list is converted to json and sent as a response.
+        return JsonResponse({"messages": result})
+    else:
+        return HttpResponse(content="Invalid request", status=400)
 
 def create_posts(request):
-    pass
+    if request.method == 'GET':
+        # This is supposed to have some ui for creating posts but i have no idea how to do that.
+        return HttpResponse(content="This is supposed to allow you to create new posts, but I dont know how to do that...")
+    else:
+        return HttpResponse(content="Invalid request", status=400)
